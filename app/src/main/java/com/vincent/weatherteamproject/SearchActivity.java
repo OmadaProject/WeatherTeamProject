a2ae7f761399eef01d8e971fb140bfa872e6fb09
despoinatasko <despoinatasko95@gmail.com>
2019-11-12 19:31:06 +0200

    Added UI for city search

---
+++ app/src/main/java/com/vincent/weatherteamproject/SearchActivity.java
@@ -0,0 +1,40 @@
+package com.vincent.weatherteamproject;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mEditText = findViewById(R.id.textEdit);
+        mButton = findViewById(R.id.button);
+    }
+
+}
